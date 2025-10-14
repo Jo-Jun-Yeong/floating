e3dc41ff93452aa6e5f39c2e0b6fa32c09c626f5
조준영 <idwnsdud1965@naver.com>
2025-10-14 23:52:09 +0900

    test commit

---
+++ Floating/src/test/java/com/board/basic/FloatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
