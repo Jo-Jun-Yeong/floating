8092cb76d7dc5b4ffe132a9161c79f51b4b4b679
조준영 <idwnsdud1965@naver.com>
2025-10-26 21:20:31 +0900

    매인 조금 수정

--- Floating/src/main/resources/templates/f_floating/src/App.tsx
+++ Floating/src/main/resources/templates/f_floating/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Test from './components/JY/Test';
+import Study from './components/JY/Study';
 
 function App() {
   return (
