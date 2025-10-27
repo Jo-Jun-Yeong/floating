6cc6dd0bce4bc9ba674fb88f37c249d18d5dfcf2
jeongjun-woo <jeongjun-woo@localhost>
2025-10-27 17:05:42 +0900

    Main 퍼블리싱 완

--- Floating/src/main/resources/templates/f_floating/src/components/JW/Main.tsx
+++ Floating/src/main/resources/templates/f_floating/src/components/JW/Main.tsx
@@ -37,18 +37,69 @@ const Main = () => {
             profileImage: profileImg
         }
     ]
+
+    interface stroy {
+        id: number;
+        userNickName: string;
+        profileImage: string;
+    }
+
+    const storys = [
+        {
+            id: 1,
+            userNickName: 'Joy21',
+            profileImage: kdhJOY
+        },
+        {
+            id: 2,
+            userNickName: 'karina00',
+            profileImage: profileImg
+        }
+    ]
+
+    interface mainStory {
+        id: number;
+        userNickName: string;
+        storyImage: string;
+        likeCount: number;
+        commentCount: number;
+    }
+
+    const mainStorys = [
+        {
+            id: 1,
+            userNickName: 'Joy21',
+            profileImage: kdhJOY,
+            storyImage: testImg,
+            timeAgo: '12시간',
+            likeCount: 124,
+            commentCount: 352
+        },
+        {
+            id: 2,
+            userNickName: 'karina000000000',
+            profileImage: profileImg,
+            storyImage: testImg,
+            timeAgo: '7시간',
+            likeCount: 1327,
+            commentCount: 1248
+        }
+    ]
     
     return (
-        <div>
+        <div style={{marginTop:'70px'}}>
             {/* 스토리 업로드 한 사람의 프로필 사진 */}
             <div className='content-left'>
                 <div className='story-upload'>
-                    <img src={kdhJOY} className='story-profile'></img><div className='content-center'>userId1</div>
-                    <img className='story-profile'></img><div className='content-center'>userId1</div>
-                    <img className='story-profile'></img><div className='content-center'>userId1</div>
-                    <img className='story-profile'></img><div className='content-center'>userId1</div>
-                    <img className='story-profile'></img><div className='content-center'>userId1</div>
-                    <img className='story-profile'></img><div className='content-center'>userId1</div>
+                    {
+                        storys.map((sty) => (
+                            <div key={sty.id}>
+                                <img src={sty.profileImage} className='story-profile'/>
+                                <div className='content-center'>
+                                    <span style={{fontSize:'9pt', fontWeight:'bold'}}>{sty.userNickName}</span>
+                                </div>
+                            </div>
+                    ))}
                 </div>
             </div>
             {/* 스토리 업로드 한 사람의 프로필 사진 */}
@@ -93,50 +144,33 @@ const Main = () => {
             }
             {/* 채팅창 */}
             {/* 게시글 */}
-            <div className='middle'>
-                <img src={profileImg} className='profile-img'></img>
-                <div style={{marginLeft:'10px', paddingTop:'5px'}} className='content-center'>userId2 <span className='gray-font'>• 10시간</span></div>
-                <div style={{marginLeft:'340px', paddingTop:'5px'}} className='content-center'> •••</div>
-            </div>
-            
-            <div className='content-center'>
-                <img className='story-img' src={testImg}></img>
-            </div>
+            {
+                mainStorys.map((mainSty) => (
+                <div key={mainSty.id}>
+                    <div className='middle'>
+                        <img src={mainSty.profileImage} className='profile-img'></img>
+                        <div style={{marginLeft:'10px', paddingTop:'5px'}} className='content-center'>{mainSty.userNickName} <span className='gray-font'>• {mainSty.timeAgo}</span></div>
+                        <div style={{marginLeft:'auto', paddingTop:'5px'}} className='content-center'> •••</div>
+                    </div>
+                    
+                    <div className='content-center'>
+                        <img className='story-img' src={testImg}></img>
+                    </div>
+                    
+                    <div className='content-center'>
+                        <div style={{fontSize:'22pt', justifyContent:'center', alignItems:'center', display:'flex', width:'500px', margin:'0 auto'}}>
+                            ♡<span style={{fontSize:'11pt', marginLeft:'3px', marginRight:'12px', paddingTop:'9px'}}>{mainSty.likeCount}</span>
+                            <img src={messageCircle}></img><span style={{fontSize:'11pt', marginLeft:'3px', paddingTop:'9px'}}>{mainSty.commentCount}</span>
+                            <img src={bookmark} style={{marginLeft:'auto'}}></img>
+                        </div>
+                    </div>
 
-            <div className='content-center'>
-                <div style={{fontSize:'22pt', justifyContent:'center', alignItems:'center', display:'flex'}}>
-                    ♡<span style={{fontSize:'11pt', marginLeft:'3px', marginRight:'12px', paddingTop:'9px'}}>150</span>
-                    <img src={messageCircle}></img><span style={{fontSize:'11pt', marginLeft:'3px', paddingTop:'9px'}}>200</span>
-                    <img src={bookmark} style={{marginLeft:'360px'}}></img>
+                    <div className='content-center'>
+                        <div style={{borderBottom:'2px solid #dbdbdb', width:'500px', margin:'20px 20px'}}></div>
+                    </div>
                 </div>
-            </div>
-
-            <div className='content-center'>
-                <div style={{borderBottom:'2px solid #dbdbdb', width:'500px', margin:'20px 20px'}}></div>
-            </div>
+            ))}
             {/* 게시글 */}
-
-            <div className='middle'>
-                <img src={profileImg} className='profile-img'></img>
-                <div style={{marginLeft:'10px', paddingTop:'5px'}} className='content-center'>userId2 <span className='gray-font'>• 10시간</span></div>
-                <div style={{marginLeft:'340px', paddingTop:'5px'}} className='content-center'> •••</div>
-            </div>
-
-            <div className='content-center'>
-                <img className='story-img' src={testImg}></img>
-            </div>
-
-            <div className='content-center'>
-                <div style={{fontSize:'22pt', justifyContent:'center', alignItems:'center', display:'flex'}}>
-                    ♡<span style={{fontSize:'11pt', marginLeft:'3px', marginRight:'12px', paddingTop:'9px'}}>150</span>
-                    <img src={messageCircle}></img><span style={{fontSize:'11pt', marginLeft:'3px', paddingTop:'9px'}}>200</span>
-                    <img src={bookmark} style={{marginLeft:'360px'}}></img>
-                </div>
-            </div>
-
-            <div className='content-center'>
-                <div style={{borderBottom:'2px solid #dbdbdb', width:'500px', margin:'20px 20px'}}></div>
-            </div>            
 
         </div>
     );
